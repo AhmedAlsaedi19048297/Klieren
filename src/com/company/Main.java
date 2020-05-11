c64e7243ccdd426d3c32a73fae9d8df5efe46b98
ahmed <ahmed_alsaedi@live.nl>
2020-05-11 19:33:47 +0200

    Added to see if this method works

---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Dus je gaat stil blijven, leuk je bent zo goed en lief voor mij!");
+    }
+}
